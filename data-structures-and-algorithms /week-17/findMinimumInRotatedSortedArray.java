d98f7923a1eb5e82f433f5133cb7aa6455340b77
Saadat Ibk <164902473+saadatibk@users.noreply.github.com>
2024-08-02 23:43:07 -0500

    Create findMinimumInRotatedSortedArray.java

---
+++ data-structures-and-algorithms /week-17/findMinimumInRotatedSortedArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        if ( nums == null || n == 0 ) return 0;
+        if ( n == 1 ) return nums[0];
+        if (nums[0] < nums[n-1]) return nums[0];
+
+        int left = 0;
+        int right = n -1;
+        while ( left <= right ){
+            int middle = left + (right - left) / 2;
+            if(middle > 0 && nums[middle - 1] > nums[middle]) return nums[middle];
+            if (nums[middle] > nums[right]){
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
