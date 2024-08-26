1a608faf46ce9af385e28066564227ef498485af
Saadat Ibk <164902473+saadatibk@users.noreply.github.com>
2024-08-26 17:42:40 -0500

    Create fibonaccimemo.java

---
+++ data-structures-and-algorithms /week-19/fibonaccimemo.java
@@ -0,0 +1,16 @@
+class Solution {
+    private int[] f;
+
+    private int fib_(int n) {
+        if (n<=1) return n;
+        if (f[n] == 0){
+            f[n] = fib_(n-1) + fib_(n-2);
+        }
+        return f[n];
+    }
+
+    public int fib(int n) {
+        f = new int[n+1];
+        return fib_(n);
+    }
+}  
